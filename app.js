48354ccae8041366652209f3eb3add909f629ab4
Todd Johnson <todd@flingo.tv>
2016-12-08 20:25:42 -0800

    set backup port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = PORT;
+var port = process.env.PORT || 3000;
         
 app.get('/', function(req, res) {
     res.sendfile('index.html');
