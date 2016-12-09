5e237996535828bb416ec70767364f52215de746
Todd Johnson <todd@flingo.tv>
2016-12-08 20:10:26 -0800

    initial install

---
+++ app.js
@@ -0,0 +1,30 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = 80;
+        
+app.get('/', function(req, res) {
+    res.sendfile('index.html');
+});
+
+io.on('connection', function(socket) {
+    console.log('a user connected');
+
+    socket.on("foo", function (data) {
+        console.log("message : ", data);
+    });
+
+    setTimeout(function () {
+        socket.disconnect(true);     
+    }, 30000);
+
+    socket.on("disconnect", function() {
+        console.log("a user disconnected"); 
+    });
+
+    
+});
+
+http.listen(port, function() {
+    console.log('listening on *:'+port);
+});
