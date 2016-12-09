da720d097947071a24d68fdda612b68ba9a335d3
Todd Johnson <todd@flingo.tv>
2016-12-09 14:31:11 -0800

    filter device based on ua

--- app.js
+++ app.js
@@ -16,12 +16,18 @@ io.on('connection', function(socket) {
         socket.handshake.headers.hasOwnProperty("user-agent")) {
             ua = socket.handshake.headers["user-agent"];
     }
-
     console.log('a user connected, socket id : '+id+", ua : "+ua);
+    
+    var filter = /^.*?\bandroid\b.*?\bbravia\b.*?$/i;
+    if (filter.test(ua)) {
+        console.log("socket id : "+id+" is on a from a blacklisted platform");
 
-    setTimeout(function () {
-        socket.disconnect(true);     
-    }, 30000);
+        setTimeout(function () {
+            socket.disconnect(true);     
+        }, 30000);
+    } else {
+        socket.emit("message", "welcome "+id);
+    }
 
     socket.on("disconnect", function() {
         console.log("a user disconnected, socket id : "+id); 
